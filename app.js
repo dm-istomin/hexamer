a4702e2f177c2ad44f48ca81ef4502e44e9a11a4
Dmitriy Istomin <dmi.istomin@gmail.com>
2016-06-26 00:37:27 -0400

    add code to make URL navbar focus when 'o' key is pressed

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const navbar = document.getElementById('navbar');
 const mainView = document.getElementById('content-view');
 
+document.onkeydown = (e) => {
+  if (e.keyCode === 79) {
+    if (document.activeElement.id != 'navbar') {
+      e.preventDefault();
+      navbar.focus();
+    }
+  }
+}
+
 navbar.addEventListener('keydown', (e) => {
-  if (e.keyCode == 13) { mainView.loadURL(e.target.value); }
-})
+  if (e.keyCode == 13) {
+    mainView.loadURL(e.target.value);
+    e.target.blur();
+  }
+  if (e.keyCode === 27) { e.target.blur(); }
+});
