a7105e3defbcc3b7249ae026230306d5574d3643
Dmitriy Istomin <dmi.istomin@gmail.com>
2016-06-26 02:19:50 -0400

    add rough implementation of scrolling with "j" and "k" hotkeys

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const navbar = document.getElementById('navbar');
 const mainView = document.getElementById('content-view');
 
+const scrollIncrement = 50;
+
+// Forward all messages from guest window to host.
+mainView.addEventListener('console-message', (e) => {
+  console.log(`webview console: ${e.message}`)
+});
+
 document.onkeydown = (e) => {
+  let navbarActive = document.activeElement.id === 'navbar';
+
   if (e.keyCode === 79) {
-    if (document.activeElement.id != 'navbar') {
+    if (!navbarActive) {
       e.preventDefault();
       navbar.focus();
     }
+  }
+
+  if (e.keyCode === 74 && !navbarActive) {
+    mainView.executeJavaScript(`window.scrollBy(0, ${scrollIncrement})`);
+  }
+
+  if (e.keyCode === 75 && !navbarActive) {
+    mainView.executeJavaScript(`window.scrollBy(0, -${scrollIncrement})`);
   }
 }
 
