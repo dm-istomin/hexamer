0c62ff2524f3d5725925add7e54f4a62dd027787
Dmitriy Istomin <dmi.istomin@gmail.com>
2016-09-05 15:43:59 -0400

    factor out navbar and webview into components

--- src/entry.js
+++ src/entry.js
@@ -3,15 +3,15 @@ require('./main.scss');
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Navbar from './components/Navbar';
+import Content from './components/Content';
+
 class App extends React.Component {
   render() {
     return (
       <main id="app">
-        <section id="navigation">
-          <input id="navbar" type="text" placeholder="URL"></input>
-        </section>
-        <webview id="content-view" preload="src/navigation.js" src="src/new-tab.html">
-        </webview>
+        <Navbar />
+        <Content />
       </main>
     );
   }
