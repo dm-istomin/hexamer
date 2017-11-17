0a758c7a3db1ac6544a53d6c646842358062558f
Dmitriy Istomin <dmi.istomin@gmail.com>
2017-11-17 13:34:58 -0500

    install react and render root App component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+        <p>
+          This is the main App component for Hexamer.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
